6188e27d3f3bc86788f94bdef1322c23b57e8615
HafiszahSaidatul <203040050@mail.unpas.ac.id>
2022-10-03 20:00:24 +0700

    Membuat Class Dan Objeck

---
+++ oop/coba.php
@@ -0,0 +1,23 @@
+<?php
+// Hafiszah Saidatul
+// 203040050
+//PRAKWEB KELAS A
+?>
+
+<?php
+
+
+class Coba{
+
+
+}
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+
+?>
