f689aa767844802af7d0d69f8aca81374fa79428
HafiszahSaidatul <203040050@mail.unpas.ac.id>
2022-10-10 15:30:23 +0700

    Menyelesaikan Video 8 DataBase Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_a_203040050/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
--- mvc/app/core/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_a_203040050/mvc/public');
-define('DEFAULT_CONTROLLER', 'Home');
-define('DEFAULT_METHOD', 'index');
